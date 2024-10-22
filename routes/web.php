a36b4d1ea2c31e15bc221cee630ebceef5691bf4
王翊安 <aud986587@gmail.com>
2024-10-23 01:32:00 +0800

    設定route接收參數

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('r2', function () {
-    return view('welcome');
+Route::get('hello/{name}', function ($name) {
+    return 'Hello, '.$name;
 });
