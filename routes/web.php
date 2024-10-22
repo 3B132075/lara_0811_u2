927f5649d071542672a5999a71f29cdb827a8293
王翊安 <aud986587@gmail.com>
2024-10-23 01:33:48 +0800

    修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello/{name}', function ($name) {
+Route::get('hello/{name?}', function ($name = 'Everybody') {
     return 'Hello, '.$name;
 });
